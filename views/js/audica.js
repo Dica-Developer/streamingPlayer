7f4d4341a07dfb507f3660e34d24dec3ba6d4f18
Martin Schaaf <mascha@ma-scha.de>
2012-12-29 20:27:44 +0100

    * fix jslint comments

--- views/js/audica.js
+++ views/js/audica.js
@@ -1,6 +1,8 @@
-$(function() {
-  $(document).on('pageinit', function() {
-    $('#stationUrl').bind('change', function() {
+"use strict";
+/*global $, document*/
+$(function () {
+  $(document).on('pageinit', function () {
+    $('#stationUrl').bind('change', function () {
       var url = $(this).val().trim();
       if (url.length > 0) {
         $('#addStationButton').removeClass('ui-disabled');
@@ -9,47 +11,47 @@ $(function() {
         $('#stationUrl').focus();
       }
     });
-    $('#addStationButton').bind('vclick', function() {
+    $('#addStationButton').bind('vclick', function () {
       var url = $('#stationUrl').val().trim();
       var name = $('#stationName').val().trim();
       if (url && url.length > 0) {
-        var params = '?url='+ url;
+        var params = '?url=' + url;
         if (name && name.length > 0) {
-          params = params + '&name='+ name;
+          params = params + '&name=' + name;
         }
-        $.post('/add'+ params, function() {
+        $.post('/add' + params, function () {
           $('#addStationDialog').dialog('close');
         });
       }
     });
   });
-  $('#prev').bind('click', function() {
-    $.get('/prev', function(response) {
+  $('#prev').bind('click', function () {
+    $.get('/prev', function (response) {
       $("#message").text(response);
     });
   });
-  $('#next').bind('click', function() {
-    $.get('/next', function(response) {
+  $('#next').bind('click', function () {
+    $.get('/next', function (response) {
       $("#message").text(response);
     });
   });
-  $('#play').bind('click', function() {
-    $.get('/play', function(response) {
+  $('#play').bind('click', function () {
+    $.get('/play', function (response) {
       $("#message").text(response);
     });
   });
-  $('#stop').bind('click', function() {
-    $.get('/stop', function(response) {
+  $('#stop').bind('click', function () {
+    $.get('/stop', function (response) {
       $("#message").text('');
     });
   });
-  $('#record').bind('click', function() {
-    $.get('/record', function(response) {
+  $('#record').bind('click', function () {
+    $.get('/record', function (response) {
       $("#message").text(response);
     });
   });
-  $('#recordStop').bind('click', function() {
-    $.get('/recordStop', function(response) {
+  $('#recordStop').bind('click', function () {
+    $.get('/recordStop', function (response) {
       $("#message").text(response);
     });
   });
